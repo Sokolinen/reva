825aa35830169b77d7e9cf86598e7d2757948e31
unknown <sokolinen@icloud.com>
2023-10-14 18:27:55 +0300

    initial

---
+++ Модуль 3 урок 2.py
@@ -0,0 +1,97 @@
+# def count_letters(word):
+#     for letter in word: 
+#          counter = 0
+#          for let in word:
+#              if let == letter:
+#                  counter += 1
+#          print(letter, counter)
+# word = 'aacd'
+# count_letters(word)
+
+
+# def count_letters(word):
+#     for letter in set(word): #мама
+#          counter = 0
+#          for let in word:
+#              if let == letter:
+#                  counter += 1
+#          print(letter, counter)
+# word = 'aacdrkgjdofijgposidhrpdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddguhweriuhogujrhgiehguh pruu prguh erghepr gprg ergfgsdfgu hsdfgserkguwrhguehrgh hguyre98tu3y458y pt89w5h8tuw4rihgy98w4h7q9845y tg8y438ty95p97gp9 858ytpwg trg yhp9wy58ty p945yth p89yc8 984y n98yvq8934y8tgy ergp 8eyrghpaieugh 98seyrgidhglsdhgskdjfhg;askdjfgh;sdkfjghs;dkfjhgs;dkfjgh s;dkfhg slskdfjgh sdlk;fjgh s;dklfjgh sldkfjgh sldkfjgh s;kdfjgh s;doitu yghsepriotu peoirt [owierjg ;kjdfnvbkcnblxk jhf;gkjhreihug ;seroigh ofigh ;soeirgtjaoeirgjfd;ogj sdroihgsijeriogj sodijaoihjeiorjgi jsidohfsiohihsiah is  hsiuhsius ss hsh shs sshss s ui s shss s s s ss  s  s ss  s ss uir;ghse;ughlsdgjsbdjhgbsjhguiheiuhguiehrguihsdijhgjserhiughriugdjgfbjhdfijbnsdfbjhsdufhgijsdiughsurnguishuhgijsndjbhdjfnbjsfhbjsnfbjihsjbfguihibjnkslblfuighiufhobhlbklairfhbvpibipvgevphbaeprygpqeihjrbiygserijgvbiuhviouh hwruithouwetiuvhuihtcouehrvtoy8urcgweirthguhtrubuhvuhrtguhrvthghrguhsehguhbuihsfgjhjfnjbczjbhpiuzdhiurhyuhsrghiurebgtbrthgygdhfbvdb;ksf;hbuidhgihspirhgiorihaojefiojasjdvjandfuighnipauejroq[                            [qojwjfoavomdkbnkjcivnivv]]'
+# count_letters(word)
+
+# def count_letter(word):
+#     count = {}
+#     for letter in word:
+#         count[letter]=count.get(letter, 0) + 1
+#     for key, value in count.items():
+#         print(key, value)
+# word = 'ddffkwwkf'
+# count_letter(word)
+
+
+
+# "C:\Users\user\код модуль 3 урок 2"
+# "C:\Users\user\код модуль 3 урок 2"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def count_letters(word):
+#     for letter in word:
+#         counter = 0
+#         for let in word:
+#             if let == letter:
+#                 counter+=1
+#         print(letter, counter)
+# word = 'аннигиляция'
+# count_letters(word)
+
+
+# def count_letters(word):
+#     for letter in set(word):
+#         counter = 0
+#         for let in word:
+#             if let == letter:
+#                 counter+=1
+#         print(letter, counter)
+# word = 'аннигиляция'
+# count_letters(word)
+
+# "C:\Users\user\GitПапка"
+
+
+
+def count_letters(word):
+    count = {}
+    for letter in word:
+        count[letter] = count.get(letter, 0) + 1
+    for key, value in count.items():
+        print(key, value)
+word = 'краска'
+count_letters(word)
+
+
